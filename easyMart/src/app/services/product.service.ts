b504408af80b383b7281379d87922b2eae19c156
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-07-11 01:16:57 +0600

    added product service

---
+++ easyMart/src/app/services/product.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, retry } from 'rxjs';
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  }
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = 'https://fakestoreapi.com';
+  
+  constructor(private http: HttpClient) { }
+  getAllProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>(`${this.apiUrl}/products`);
+  }
+  getProductsById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/products/${id}`);
+  }
+}
