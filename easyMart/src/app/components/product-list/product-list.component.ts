91510096f04c059377f019504ae698e9b240914f
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-07-13 16:00:35 +0600

    get all products

---
+++ easyMart/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Product, ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.scss'
+})
+export class ProductListComponent {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService){}
+  ngOnInit(): void{
+    this.productService.getAllProducts().subscribe(data => {
+      this.products = data;
+      console.log('Products loaded:', this.products);
+    })
+  }
+}
